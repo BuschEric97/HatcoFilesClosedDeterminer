cf4a92c5a3ab848abe9e18bc9e1d90bfa43bedda
Eric Busch <buscheric97@gmail.com>
2019-08-16 16:38:29 -0500

    Changed compilation settings to compile to only x64 processors

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -37,49 +37,46 @@
             // 
             // MLS_Input_File
             // 
-            this.MLS_Input_File.Location = new System.Drawing.Point(41, 64);
-            this.MLS_Input_File.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MLS_Input_File.Location = new System.Drawing.Point(31, 52);
             this.MLS_Input_File.Name = "MLS_Input_File";
-            this.MLS_Input_File.Size = new System.Drawing.Size(439, 22);
+            this.MLS_Input_File.Size = new System.Drawing.Size(527, 20);
             this.MLS_Input_File.TabIndex = 0;
-            this.MLS_Input_File.Text = "C:\\Users\\Origami1105\\Desktop\\MLSData.xlsx";
+            this.MLS_Input_File.Text = "C:\\Users\\Origami1105\\source\\repos\\BuschEric97\\HatcoFilesClosedDeterminer\\TestFile" +
+    "s\\MLSData.xlsx";
             // 
             // AIM_Input_File
             // 
-            this.AIM_Input_File.Location = new System.Drawing.Point(41, 150);
-            this.AIM_Input_File.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AIM_Input_File.Location = new System.Drawing.Point(31, 122);
             this.AIM_Input_File.Name = "AIM_Input_File";
-            this.AIM_Input_File.Size = new System.Drawing.Size(439, 22);
+            this.AIM_Input_File.Size = new System.Drawing.Size(527, 20);
             this.AIM_Input_File.TabIndex = 1;
-            this.AIM_Input_File.Text = "C:\\Users\\Origami1105\\Desktop\\AIMData.xlsx";
+            this.AIM_Input_File.Text = "C:\\Users\\Origami1105\\source\\repos\\BuschEric97\\HatcoFilesClosedDeterminer\\TestFile" +
+    "s\\AIMData.xlsx";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 41);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(31, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 17);
+            this.label1.Size = new System.Drawing.Size(141, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "MLS Input/Output FIle Path:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(41, 127);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(31, 103);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.Size = new System.Drawing.Size(106, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "AIM+ Input File Path:";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(644, 64);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(627, 52);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(296, 108);
+            this.button1.Size = new System.Drawing.Size(222, 88);
             this.button1.TabIndex = 6;
             this.button1.Text = "Run";
             this.button1.UseVisualStyleBackColor = true;
@@ -87,16 +84,15 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1067, 228);
+            this.ClientSize = new System.Drawing.Size(904, 185);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.AIM_Input_File);
             this.Controls.Add(this.MLS_Input_File);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Hatco Files Closed Determiner";
